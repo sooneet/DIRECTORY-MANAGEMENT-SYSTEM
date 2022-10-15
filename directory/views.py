10e5c6dce735b5f01730097df690b9ae06983e15
sooneet <85282640+sooneet@users.noreply.github.com>
2022-10-15 20:38:50 +0545

    login page

--- directory/views.py
+++ directory/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth import authenticate,login,logout
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def Login(request):
+    error = ''
+    if request.method == 'POST':
+        u = request.POST['uname']
+        p = request.POST['pwd']
+        user = authenticate(username=u,password=p)
+        try:
+            if user.is_staff:
+                login(request,user)
+                error = 'no'
+            else:
+                error = 'yes'    
+        except:
+            error = 'yes' 
+    return render(request,'login.html',locals())
